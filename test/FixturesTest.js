f6f030664345d567b1e03fe6cfbbbd1f3f86e953
Evgeny RevxChain <104643965+RevxChain@users.noreply.github.com>
2026-03-02 17:35:43 +0100

    add {UniswapV3DeploymentFixture} with custom WETH

---
+++ test/FixturesTest.js
@@ -0,0 +1,50 @@
+const { UniswapV3DeploymentFixture, createUniswapV3DeploymentFixtureCustomWETH } = require("./UniswapV3DeploymentFixture");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { withDecimals } = require("./Utils");
+const { expect } = require("chai");
+
+describe("FixturesTest", function () {
+    it("Default fixture", async function () {
+        const { weth, tokenDescriptor, positionManager, swapRouter01, swapRouter02, quoter01, quoter02 } = await loadFixture(UniswapV3DeploymentFixture);
+
+        expect(await tokenDescriptor.WETH9()).to.equal(weth.target);
+        expect(await positionManager.WETH9()).to.equal(weth.target);
+        expect(await swapRouter01.WETH9()).to.equal(weth.target);
+        expect(await swapRouter02.WETH9()).to.equal(weth.target);
+        expect(await quoter01.WETH9()).to.equal(weth.target);
+        expect(await quoter02.WETH9()).to.equal(weth.target);
+
+        expect(await weth.name()).to.equal("Wrapped ETH");
+        expect(await weth.symbol()).to.equal("WETH");
+        expect(await weth.decimals()).to.equal(18n);
+        expect(await weth.totalSupply()).to.equal(0n);
+    });
+
+    it("Custom WETH fixture", async function () {
+        const [user] = await ethers.getSigners();
+
+        const WETH = await ethers.getContractFactory("WETH", user);
+        const wethPreDeploy = await WETH.deploy();
+        await wethPreDeploy.waitForDeployment();
+
+        await wethPreDeploy.connect(user).deposit({ value: withDecimals("9000") });
+
+        const UniswapV3DeploymentFixtureCustomWETH = createUniswapV3DeploymentFixtureCustomWETH(wethPreDeploy);
+        const { weth, tokenDescriptor, positionManager, swapRouter01, swapRouter02, quoter01, quoter02 } = await loadFixture(UniswapV3DeploymentFixtureCustomWETH);
+
+        expect(wethPreDeploy).to.equal(weth);
+        expect(wethPreDeploy.target).to.equal(weth.target);
+
+        expect(await tokenDescriptor.WETH9()).to.equal(weth.target);
+        expect(await positionManager.WETH9()).to.equal(weth.target);
+        expect(await swapRouter01.WETH9()).to.equal(weth.target);
+        expect(await swapRouter02.WETH9()).to.equal(weth.target);
+        expect(await quoter01.WETH9()).to.equal(weth.target);
+        expect(await quoter02.WETH9()).to.equal(weth.target);
+
+        expect(await weth.name()).to.equal("Wrapped ETH");
+        expect(await weth.symbol()).to.equal("WETH");
+        expect(await weth.decimals()).to.equal(18n);
+        expect(await weth.totalSupply()).to.equal(withDecimals("9000"));
+    });
+});
--- test/UniswapV3DeploymentFixture.js
+++ test/UniswapV3DeploymentFixture.js
@@ -1,101 +1,116 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { zeroAddress } = require("./Utils");
 
 async function UniswapV3DeploymentFixture() {
     const [, , , , , , , , , , , , , , , , , , , uniswapV3Deployer] = await ethers.getSigners();
 
-    const nativeCurrencyLabel = "0x4554480000000000000000000000000000000000000000000000000000000000";
-
     const WETH = await ethers.getContractFactory("WETH", uniswapV3Deployer);
     const weth = await WETH.deploy();
     await weth.waitForDeployment();
 
-    const UniswapV3Factory = new ethers.ContractFactory(
-        require('@uniswap/v3-core-0.7/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json').abi,
-        require('@uniswap/v3-core-0.7/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json').bytecode,
-        uniswapV3Deployer
-    );
-    const uniswapFactory = await UniswapV3Factory.deploy();
-    await uniswapFactory.waitForDeployment();
+    const UniswapV3DeploymentFixtureCustomWETH = createUniswapV3DeploymentFixtureCustomWETH(weth);
 
-    await uniswapFactory.connect(uniswapV3Deployer).enableFeeAmount(100, 1);
-
-    const NFTDescriptor = new ethers.ContractFactory(
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/libraries/NFTDescriptor.sol/NFTDescriptor.json').abi,
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/libraries/NFTDescriptor.sol/NFTDescriptor.json').bytecode,
-        uniswapV3Deployer
-    );
-    const NFTDescriptorLibrary = await NFTDescriptor.deploy();
-    await NFTDescriptorLibrary.waitForDeployment();
-
-    const NonfungibleTokenPositionDescriptor = new ethers.ContractFactory(
-        require(
-            '@uniswap/v3-periphery-0.7/artifacts/contracts/NonfungibleTokenPositionDescriptor.sol/NonfungibleTokenPositionDescriptor.json'
-        ).abi,
-        require(
-            '@uniswap/v3-periphery-0.7/artifacts/contracts/NonfungibleTokenPositionDescriptor.sol/NonfungibleTokenPositionDescriptor.json'
-        ).bytecode.replace("__$cea9be979eee3d87fb124d6cbb244bb0b5$__", NFTDescriptorLibrary.target.slice(2)),
-        uniswapV3Deployer
-    );
-    const tokenDescriptor = await NonfungibleTokenPositionDescriptor.deploy(weth.target, nativeCurrencyLabel);
-    await tokenDescriptor.waitForDeployment();
-
-    const NonfungiblePositionManager = new ethers.ContractFactory(
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json').abi,
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json').bytecode,
-        uniswapV3Deployer
-    );
-    const positionManager = await NonfungiblePositionManager.deploy(uniswapFactory.target, weth.target, tokenDescriptor.target);
-    await positionManager.waitForDeployment();
-
-    const SwapRouter01 = new ethers.ContractFactory(
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/SwapRouter.sol/SwapRouter.json').abi,
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/SwapRouter.sol/SwapRouter.json').bytecode,
-        uniswapV3Deployer
-    );
-    const swapRouter01 = await SwapRouter01.deploy(uniswapFactory.target, weth.target);
-    await swapRouter01.waitForDeployment();
-
-    const SwapRouter02 = new ethers.ContractFactory(
-        require('@uniswap/swap-router-contracts/artifacts/contracts/SwapRouter02.sol/SwapRouter02.json').abi,
-        require('@uniswap/swap-router-contracts/artifacts/contracts/SwapRouter02.sol/SwapRouter02.json').bytecode,
-        uniswapV3Deployer
-    );
-    const swapRouter02 = await SwapRouter02.deploy(zeroAddress, uniswapFactory.target, positionManager.target, weth.target);
-    await swapRouter02.waitForDeployment();
-
-    const QuoterV1 = new ethers.ContractFactory(
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/Quoter.sol/Quoter.json').abi,
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/Quoter.sol/Quoter.json').bytecode,
-        uniswapV3Deployer
-    );
-    const quoter01 = await QuoterV1.deploy(uniswapFactory.target, weth.target);
-    await quoter01.waitForDeployment();
-
-    const QuoterV2 = new ethers.ContractFactory(
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json').abi,
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json').bytecode,
-        uniswapV3Deployer
-    );
-    const quoter02 = await QuoterV2.deploy(uniswapFactory.target, weth.target);
-    await quoter02.waitForDeployment();
-
-    const TickLens = new ethers.ContractFactory(
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/TickLens.sol/TickLens.json').abi,
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/TickLens.sol/TickLens.json').bytecode,
-        uniswapV3Deployer
-    );
-    const tickLens = await TickLens.deploy();
-    await tickLens.waitForDeployment();
-
-    const UniswapInterfaceMulticall = new ethers.ContractFactory(
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/UniswapInterfaceMulticall.sol/UniswapInterfaceMulticall.json').abi,
-        require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/UniswapInterfaceMulticall.sol/UniswapInterfaceMulticall.json').bytecode,
-        uniswapV3Deployer
-    );
-    const interfaceMulticall = await UniswapInterfaceMulticall.deploy();
-    await interfaceMulticall.waitForDeployment();
-
-    return { uniswapV3Deployer, weth, uniswapFactory, tokenDescriptor, positionManager, swapRouter01, swapRouter02, quoter01, quoter02, tickLens, interfaceMulticall };
+    return await loadFixture(UniswapV3DeploymentFixtureCustomWETH);
 }
 
-module.exports = { UniswapV3DeploymentFixture };
+function createUniswapV3DeploymentFixtureCustomWETH(weth) {
+    async function UniswapV3DeploymentFixtureCustomWETH() {
+        const [, , , , , , , , , , , , , , , , , , , uniswapV3Deployer] = await ethers.getSigners();
+
+        const nativeCurrencyLabel = "0x4554480000000000000000000000000000000000000000000000000000000000";
+
+        const UniswapV3Factory = new ethers.ContractFactory(
+            require('@uniswap/v3-core-0.7/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json').abi,
+            require('@uniswap/v3-core-0.7/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json').bytecode,
+            uniswapV3Deployer
+        );
+        const uniswapFactory = await UniswapV3Factory.deploy();
+        await uniswapFactory.waitForDeployment();
+
+        await uniswapFactory.connect(uniswapV3Deployer).enableFeeAmount(100, 1);
+
+        const NFTDescriptor = new ethers.ContractFactory(
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/libraries/NFTDescriptor.sol/NFTDescriptor.json').abi,
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/libraries/NFTDescriptor.sol/NFTDescriptor.json').bytecode,
+            uniswapV3Deployer
+        );
+        const NFTDescriptorLibrary = await NFTDescriptor.deploy();
+        await NFTDescriptorLibrary.waitForDeployment();
+
+        const NonfungibleTokenPositionDescriptor = new ethers.ContractFactory(
+            require(
+                '@uniswap/v3-periphery-0.7/artifacts/contracts/NonfungibleTokenPositionDescriptor.sol/NonfungibleTokenPositionDescriptor.json'
+            ).abi,
+            require(
+                '@uniswap/v3-periphery-0.7/artifacts/contracts/NonfungibleTokenPositionDescriptor.sol/NonfungibleTokenPositionDescriptor.json'
+            ).bytecode.replace("__$cea9be979eee3d87fb124d6cbb244bb0b5$__", NFTDescriptorLibrary.target.slice(2)),
+            uniswapV3Deployer
+        );
+        const tokenDescriptor = await NonfungibleTokenPositionDescriptor.deploy(weth.target, nativeCurrencyLabel);
+        await tokenDescriptor.waitForDeployment();
+
+        const NonfungiblePositionManager = new ethers.ContractFactory(
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json').abi,
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json').bytecode,
+            uniswapV3Deployer
+        );
+        const positionManager = await NonfungiblePositionManager.deploy(uniswapFactory.target, weth.target, tokenDescriptor.target);
+        await positionManager.waitForDeployment();
+
+        const SwapRouter01 = new ethers.ContractFactory(
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/SwapRouter.sol/SwapRouter.json').abi,
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/SwapRouter.sol/SwapRouter.json').bytecode,
+            uniswapV3Deployer
+        );
+        const swapRouter01 = await SwapRouter01.deploy(uniswapFactory.target, weth.target);
+        await swapRouter01.waitForDeployment();
+
+        const SwapRouter02 = new ethers.ContractFactory(
+            require('@uniswap/swap-router-contracts/artifacts/contracts/SwapRouter02.sol/SwapRouter02.json').abi,
+            require('@uniswap/swap-router-contracts/artifacts/contracts/SwapRouter02.sol/SwapRouter02.json').bytecode,
+            uniswapV3Deployer
+        );
+        const swapRouter02 = await SwapRouter02.deploy(zeroAddress, uniswapFactory.target, positionManager.target, weth.target);
+        await swapRouter02.waitForDeployment();
+
+        const QuoterV1 = new ethers.ContractFactory(
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/Quoter.sol/Quoter.json').abi,
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/Quoter.sol/Quoter.json').bytecode,
+            uniswapV3Deployer
+        );
+        const quoter01 = await QuoterV1.deploy(uniswapFactory.target, weth.target);
+        await quoter01.waitForDeployment();
+
+        const QuoterV2 = new ethers.ContractFactory(
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json').abi,
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json').bytecode,
+            uniswapV3Deployer
+        );
+        const quoter02 = await QuoterV2.deploy(uniswapFactory.target, weth.target);
+        await quoter02.waitForDeployment();
+
+        const TickLens = new ethers.ContractFactory(
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/TickLens.sol/TickLens.json').abi,
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/TickLens.sol/TickLens.json').bytecode,
+            uniswapV3Deployer
+        );
+        const tickLens = await TickLens.deploy();
+        await tickLens.waitForDeployment();
+
+        const UniswapInterfaceMulticall = new ethers.ContractFactory(
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/UniswapInterfaceMulticall.sol/UniswapInterfaceMulticall.json').abi,
+            require('@uniswap/v3-periphery-0.7/artifacts/contracts/lens/UniswapInterfaceMulticall.sol/UniswapInterfaceMulticall.json').bytecode,
+            uniswapV3Deployer
+        );
+        const interfaceMulticall = await UniswapInterfaceMulticall.deploy();
+        await interfaceMulticall.waitForDeployment();
+
+        return {
+            uniswapV3Deployer, weth, uniswapFactory, tokenDescriptor, positionManager, swapRouter01, swapRouter02, quoter01, quoter02, tickLens, interfaceMulticall
+        };
+    }
+
+    return UniswapV3DeploymentFixtureCustomWETH;
+}
+
+module.exports = { UniswapV3DeploymentFixture, createUniswapV3DeploymentFixtureCustomWETH };
