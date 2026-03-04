0c818f34d436eff2d661b1f0ec6d2691aea090fa
Evgeny RevxChain <104643965+RevxChain@users.noreply.github.com>
2026-03-04 11:59:30 +0100

    add {UniswapV3MainnetForkSetup} test
    + unify solidity version for mocks

--- contracts/mocks/UniswapV3UtilsMock.sol
+++ contracts/mocks/UniswapV3UtilsMock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.20;
 
 import {TickMath} from "@uniswap/v3-core/contracts/libraries/TickMath.sol";
 
--- test/FixturesTest.js
+++ test/FixturesTest.js
@@ -1,54 +1,79 @@
-const { UniswapV3DeploymentFixture, createUniswapV3DeploymentFixtureCustomWETH } = require("./UniswapV3DeploymentFixture");
+const { UniswapV3MainnetForkSetup, UniswapV3DeploymentFixture, createUniswapV3DeploymentFixtureCustomWETH } = require("./UniswapV3DeploymentFixture");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { withDecimals } = require("./Utils");
 const { expect } = require("chai");
 
-if (require('../hardhat.config.js').networks.hardhat.forking.enabled) {
-    return;
-}
-
 describe("FixturesTest", function () {
-    it("Default fixture", async function () {
-        const { weth, tokenDescriptor, positionManager, swapRouter01, swapRouter02, quoter01, quoter02 } = await loadFixture(UniswapV3DeploymentFixture);
+    if (require('../hardhat.config.js').networks.hardhat.forking.enabled) {
+        it("Mainnet fork setup", async function () {
+            const {
+                weth, uniswapFactory, descriptorLibrary, tokenDescriptor, positionManager, swapRouter01, swapRouter02, quoter01, quoter02,
+                tickLens, multicall, multicall2, permit2
+            } = await UniswapV3MainnetForkSetup(1);
 
-        expect(await tokenDescriptor.WETH9()).to.equal(weth.target);
-        expect(await positionManager.WETH9()).to.equal(weth.target);
-        expect(await swapRouter01.WETH9()).to.equal(weth.target);
-        expect(await swapRouter02.WETH9()).to.equal(weth.target);
-        expect(await quoter01.WETH9()).to.equal(weth.target);
-        expect(await quoter02.WETH9()).to.equal(weth.target);
+            const [, , , , , , , , , , , , , , , , , , , localSigner] = await ethers.getSigners();
 
-        expect(await weth.name()).to.equal("Wrapped Ether");
-        expect(await weth.symbol()).to.equal("WETH");
-        expect(await weth.decimals()).to.equal(18n);
-        expect(await weth.totalSupply()).to.equal(0n);
-    });
+            expect(await uniswapFactory.owner()).to.not.equal(localSigner);
+            expect(await ethers.provider.getCode(descriptorLibrary.target)).to.not.equal("0x");
+            expect(await tokenDescriptor.WETH9()).to.equal(weth.target);
+            expect(await positionManager.WETH9()).to.equal(weth.target);
+            expect(await swapRouter01.WETH9()).to.equal(weth.target);
+            expect(await swapRouter02.WETH9()).to.equal(weth.target);
+            expect(await quoter01.WETH9()).to.equal(weth.target);
+            expect(await quoter02.WETH9()).to.equal(weth.target);
+            expect(await tickLens.getPopulatedTicksInWord("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640", 0n)).to.not.eql([]);
+            expect(await ethers.provider.getCode(multicall.target)).to.not.equal("0x");
+            expect(await ethers.provider.getCode(multicall2.target)).to.not.equal("0x");
+            expect(await ethers.provider.getCode(permit2.target)).to.not.equal("0x");
 
-    it("Custom WETH fixture", async function () {
-        const [user] = await ethers.getSigners();
+            expect(await weth.name()).to.equal("Wrapped Ether");
+            expect(await weth.symbol()).to.equal("WETH");
+            expect(await weth.decimals()).to.equal(18n);
+            expect(await weth.totalSupply()).to.above(0n);
+        });
+    } else {
+        it("Default fixture", async function () {
+            const { weth, tokenDescriptor, positionManager, swapRouter01, swapRouter02, quoter01, quoter02 } = await loadFixture(UniswapV3DeploymentFixture);
 
-        const WETH9 = new ethers.ContractFactory(require('../build/WETH9.json').abi, require('../build/WETH9.json').bytecode, user);
-        const wethPreDeploy = await WETH9.deploy();
-        await wethPreDeploy.waitForDeployment();
+            expect(await tokenDescriptor.WETH9()).to.equal(weth.target);
+            expect(await positionManager.WETH9()).to.equal(weth.target);
+            expect(await swapRouter01.WETH9()).to.equal(weth.target);
+            expect(await swapRouter02.WETH9()).to.equal(weth.target);
+            expect(await quoter01.WETH9()).to.equal(weth.target);
+            expect(await quoter02.WETH9()).to.equal(weth.target);
 
-        await wethPreDeploy.connect(user).deposit({ value: withDecimals("9000") });
+            expect(await weth.name()).to.equal("Wrapped Ether");
+            expect(await weth.symbol()).to.equal("WETH");
+            expect(await weth.decimals()).to.equal(18n);
+            expect(await weth.totalSupply()).to.equal(0n);
+        });
 
-        const UniswapV3DeploymentFixtureCustomWETH = createUniswapV3DeploymentFixtureCustomWETH(wethPreDeploy);
-        const { weth, tokenDescriptor, positionManager, swapRouter01, swapRouter02, quoter01, quoter02 } = await loadFixture(UniswapV3DeploymentFixtureCustomWETH);
+        it("Custom WETH fixture", async function () {
+            const [user] = await ethers.getSigners();
 
-        expect(wethPreDeploy).to.equal(weth);
-        expect(wethPreDeploy.target).to.equal(weth.target);
+            const WETH9 = new ethers.ContractFactory(require('../build/WETH9.json').abi, require('../build/WETH9.json').bytecode, user);
+            const wethPreDeploy = await WETH9.deploy();
+            await wethPreDeploy.waitForDeployment();
 
-        expect(await tokenDescriptor.WETH9()).to.equal(weth.target);
-        expect(await positionManager.WETH9()).to.equal(weth.target);
-        expect(await swapRouter01.WETH9()).to.equal(weth.target);
-        expect(await swapRouter02.WETH9()).to.equal(weth.target);
-        expect(await quoter01.WETH9()).to.equal(weth.target);
-        expect(await quoter02.WETH9()).to.equal(weth.target);
+            await wethPreDeploy.connect(user).deposit({ value: withDecimals("9000") });
 
-        expect(await weth.name()).to.equal("Wrapped Ether");
-        expect(await weth.symbol()).to.equal("WETH");
-        expect(await weth.decimals()).to.equal(18n);
-        expect(await weth.totalSupply()).to.equal(withDecimals("9000"));
-    });
+            const UniswapV3DeploymentFixtureCustomWETH = createUniswapV3DeploymentFixtureCustomWETH(wethPreDeploy);
+            const { weth, tokenDescriptor, positionManager, swapRouter01, swapRouter02, quoter01, quoter02 } = await loadFixture(UniswapV3DeploymentFixtureCustomWETH);
+
+            expect(wethPreDeploy).to.equal(weth);
+            expect(wethPreDeploy.target).to.equal(weth.target);
+
+            expect(await tokenDescriptor.WETH9()).to.equal(weth.target);
+            expect(await positionManager.WETH9()).to.equal(weth.target);
+            expect(await swapRouter01.WETH9()).to.equal(weth.target);
+            expect(await swapRouter02.WETH9()).to.equal(weth.target);
+            expect(await quoter01.WETH9()).to.equal(weth.target);
+            expect(await quoter02.WETH9()).to.equal(weth.target);
+
+            expect(await weth.name()).to.equal("Wrapped Ether");
+            expect(await weth.symbol()).to.equal("WETH");
+            expect(await weth.decimals()).to.equal(18n);
+            expect(await weth.totalSupply()).to.equal(withDecimals("9000"));
+        });
+    }
 });
